db4b6cdc69f3aacc7f74216be5d3cde70aa3a547
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-10 17:26:39 -0300

    feat: added route to create championship

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import { CreateChampionshipController } from './modules/championship/useCases/createChampionship/CreateChampionshipController';
+
 const routes = Router();
+
+const createChampionshipController = new CreateChampionshipController();
+
+routes.post('/championship', createChampionshipController.handle);
 
 export { routes };
