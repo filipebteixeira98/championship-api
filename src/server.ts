4669518c57cd33716392838c780781b706ae043f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-10 16:46:25 -0300

    chore: created router

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import { routes } from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.send();
-});
+app.use(routes);
 
 app.listen(3333, () =>
   console.log('🚀 Server is running at http://localhost:3333')
